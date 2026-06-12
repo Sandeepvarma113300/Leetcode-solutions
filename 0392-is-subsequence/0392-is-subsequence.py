a7002f29604401d5ac57902abaedc0574b7d735e
B VENKATA SANDEEP VARMA <23A91A0577@aec.edu.in>
2026-06-12 13:18:36 +0530

    Time: 0 ms (100%), Space: 19.3 MB (64.99%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        l=0
+        r=0
+        while l<len(s) and r<len(t):
+            if s[l]==t[r]:
+                l=l+1
+                r=r+1
+            else:
+                r=r+1
+        if l==len(s):
+            return True
+        else:
+            return False
