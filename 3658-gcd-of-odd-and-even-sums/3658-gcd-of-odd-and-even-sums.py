571c709e69f82270f4e48da7f4907e12b877ec54
B VENKATA SANDEEP VARMA <23A91A0577@aec.edu.in>
2026-07-15 21:46:34 +0530

    Time: 0 ms (100%), Space: 19.2 MB (53.03%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -1,13 +1,4 @@
 import math
 class Solution:
     def gcdOfOddEvenSums(self, n: int) -> int:
-        s1=0
-        s2=0
-        r=(n+n)
-        for i in range(r+1):
-            if i%2==0:
-                s1=s1+i
-            else:
-                s2=s2+i
-        return math.gcd(s1,s2)
-           
+        return math.gcd(n)
