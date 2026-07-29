1cde64b428acdec3925f138cff8a197dec3f159f
B VENKATA SANDEEP VARMA <23A91A0577@aec.edu.in>
2026-07-29 13:18:23 +0530

    Time: 4 ms (79.49%), Space: 13.5 MB (76.66%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,10 +1,8 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        b=[]
+        left=0
         for i in range(len(nums)):
             if nums[i]!=0:
-                b.append(nums[i])
-        for i in range(len(b)):
-            nums[i]=b[i]
-        for i in range(len(b),len(nums)):
-            nums[i]=0
+                nums[left],nums[i]=nums[i],nums[left]
+                left+=1
+        return nums
