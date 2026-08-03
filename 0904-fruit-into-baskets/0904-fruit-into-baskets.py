cc84b98968da83015c476b0d29dbad356c10255c
B VENKATA SANDEEP VARMA <23A91A0577@aec.edu.in>
2026-08-03 10:48:01 +0530

    Time: 198 ms (27.69%), Space: 25.9 MB (64.06%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -2,17 +2,18 @@ class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         left=0
         freq={}
-        max_len=0
-        for right in range(len(fruits)):
-            if fruits[right] in freq:
-                freq[fruits[right]]+=1
+        k=2
+        mx=0
+        for i in range(len(fruits)):
+            if fruits[i] in freq:
+                freq[fruits[i]]+=1
             else:
-                freq[fruits[right]]=1
-            while len(freq)>2:
+                freq[fruits[i]]=1
+            while len(freq)>k:
                 freq[fruits[left]]-=1
-                if freq[fruits[left]] ==0:
+                if freq[fruits[left]]==0:
                     del freq[fruits[left]]
                 left=left+1
-            current_length=right-left+1
-            max_len=max(max_len,current_length)
-        return max_len
+            curr=i-left+1
+            mx=max(mx,curr)
+        return mx
