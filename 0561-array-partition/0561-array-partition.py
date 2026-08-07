cc2989ba08a7414f15855af596fdbd4cbf9a5c81
B VENKATA SANDEEP VARMA <23A91A0577@aec.edu.in>
2026-08-07 22:36:46 +0530

    Time: 35 ms (12.84%), Space: 21.5 MB (60.13%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.py
@@ -0,0 +1,10 @@
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        i=0
+        ans=0
+        while i<len(nums):
+            ans=ans+nums[i]
+            i+=2
+        return ans
+            
